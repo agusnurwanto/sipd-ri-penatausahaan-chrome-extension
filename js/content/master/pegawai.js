19e4de813f8c951e4ce4a3bfcf5121a5f3e5beaf
Agus Nurwanto <agusnurwantomuslim@gmail.com>
2024-01-18 21:08:00 +0700

    optimasi get skpd saat singkron pegawai

--- js/content/master/pegawai.js
+++ js/content/master/pegawai.js
@@ -14,45 +14,53 @@ function get_pegawai(opsi, page=1, limit=50){
 			url: config.service_url+'pegawai/strict/pegawai?page='+page+'&limit='+limit,
 			type: 'get',
 			success: function(data){
-				// jika user yang login adalah BUD
-				if(
-					page == 1
-					&& _token.id_role == 9
-				){
-					data.push({
-				        "id": '',
-				        "id_daerah": _token.id_daerah,
-				        "id_skpd": 0,
-				        "id_user": _token.id_user,
-				        "id_role": _token.id_role,
-				        "nama_role": "BENDAHARA UMUM DAERAH",
-				        "tahun_pegawai": _token.tahun,
-				        "id_pegawai_kpa": 0,
-				        "status": "",
-				        "id_pegawai_ref": "0",
-				        "id_user_kpa": 0,
-				        "nama_user": "",
-				        "nip_user": ""
-					})
-				}
-				var last = data.length-1;
-				data.reduce(function(sequence, nextData){
-		            return sequence.then(function(current_data){
-		        		return new Promise(function(resolve_reduce, reject_reduce){
-		        			relayAjaxApiKey({
-								url: config.service_url+'auth/strict/user-manager/'+current_data.id_user,
-								type: 'get',
-								success: function(user){
-		        					pesan_loading('Get detail pegawai '+user.nama_user);
-									get_view_skpd().then(function(skpd){
-										console.log(skpd);
+				get_view_skpd().then(function(skpd){
+					var skpd_all = {};
+					skpd.map(function(b, i){
+						skpd_all[b.id_skpd] = b;
+					});
+					console.log('skpd_all', skpd_all);
+					// jika user yang login adalah BUD
+					if(
+						page == 1
+						&& _token.id_role == 9
+					){
+						data.push({
+					        "id": '',
+					        "id_daerah": _token.id_daerah,
+					        "id_skpd": 0,
+					        "id_user": _token.id_user,
+					        "id_role": _token.id_role,
+					        "nama_role": "BENDAHARA UMUM DAERAH",
+					        "tahun_pegawai": _token.tahun,
+					        "id_pegawai_kpa": 0,
+					        "status": "",
+					        "id_pegawai_ref": "0",
+					        "id_user_kpa": 0,
+					        "nama_user": "",
+					        "nip_user": ""
+						})
+					}
+					var last = data.length-1;
+					data.reduce(function(sequence, nextData){
+			            return sequence.then(function(current_data){
+			        		return new Promise(function(resolve_reduce, reject_reduce){
+			        			relayAjaxApiKey({
+									url: config.service_url+'auth/strict/user-manager/'+current_data.id_user,
+									type: 'get',
+									success: function(user){
+			        					pesan_loading('Get detail pegawai '+user.nama_user);
 										current_data.detail_user = user;
 										current_data.skpd = {
 											'idSkpd': current_data.id_skpd,
-											'namaSkpd': skpd.nama_skpd,
-											'kodeSkpd': skpd.kode_skpd,
+											'namaSkpd': '',
+											'kodeSkpd': '',
 											'idDaerah': current_data.id_daerah
 										};
+										if(skpd_all[current_data.id_skpd]){
+											current_data.skpd.namaSkpd = skpd_all[current_data.id_skpd].nama_skpd;
+											current_data.skpd.kodeSkpd = skpd_all[current_data.id_skpd].kode_skpd;
+										}
 										current_data.userName = user.nip_user;
 										current_data.nip = user.nip_user;
 										current_data.fullName = user.nama_user;
@@ -87,54 +95,54 @@ function get_pegawai(opsi, page=1, limit=50){
 										current_data.alamat = user.alamat;
 										opsi.data.push(current_data);
 										resolve_reduce(nextData);
-									});
+									}
+								});
+			        		})
+			                .catch(function(e){
+			                    console.log(e);
+			                    return Promise.resolve(nextData);
+			                });
+			            })
+			            .catch(function(e){
+			                console.log(e);
+			                return Promise.resolve(nextData);
+			            });
+			        }, Promise.resolve(data[last]))
+			        .then(function(data_last){
+			        	var data_back = {
+						    message:{
+						        type: "get-url",
+						        content: {
+								    url: config.url_server_lokal,
+								    type: 'post',
+								    data: { 
+										action: 'singkron_user_penatausahaan',
+										tahun_anggaran: _token.tahun,
+										api_key: config.api_key,
+										type: 'ri',
+										data_user: opsi.data
+									},
+					    			return: false
 								}
-							});
-		        		})
-		                .catch(function(e){
-		                    console.log(e);
-		                    return Promise.resolve(nextData);
-		                });
-		            })
-		            .catch(function(e){
-		                console.log(e);
-		                return Promise.resolve(nextData);
-		            });
-		        }, Promise.resolve(data[last]))
-		        .then(function(data_last){
-		        	var data_back = {
-					    message:{
-					        type: "get-url",
-					        content: {
-							    url: config.url_server_lokal,
-							    type: 'post',
-							    data: { 
-									action: 'singkron_user_penatausahaan',
-									tahun_anggaran: _token.tahun,
-									api_key: config.api_key,
-									type: 'ri',
-									data_user: opsi.data
-								},
-				    			return: false
-							}
-					    }
-					};
-					chrome.runtime.sendMessage(data_back, function(response) {
-					    console.log('responeMessage', response);
-					});
-
-					if(data.length >= limit){
-						// dikosongkan lagi setelah data dikirim ke lokal
-						opsi.data = [];
-						page++;
-						get_pegawai(opsi, page, limit)
-						.then(function(newdata){
-							resolve(newdata);
+						    }
+						};
+						chrome.runtime.sendMessage(data_back, function(response) {
+						    console.log('responeMessage', response);
 						});
-					}else{
-						resolve(opsi.data);
-					}
-		        });
+
+						if(data.length >= limit){
+							// dikosongkan lagi setelah data dikirim ke lokal
+							opsi.data = [];
+							page++;
+							get_pegawai(opsi, page, limit)
+							.then(function(newdata){
+								resolve(newdata);
+							});
+						}else{
+							resolve(opsi.data);
+						}
+			        });
+				});
 			}
 		});
 	});
