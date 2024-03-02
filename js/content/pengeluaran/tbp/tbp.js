7dc669a2008a686c2932d31a363983c16771fa01
Saiful Bangil <saifulbangil@gmail.com>
2024-03-03 03:03:06 +0700

    singkron tbp

--- js/content/pengeluaran/tbp/tbp.js
+++ js/content/pengeluaran/tbp/tbp.js
@@ -4,7 +4,8 @@ function singkron_tbp_lokal() {
 	var status = 'aktif';
 	pesan_loading('Get data TBP, status='+status);
 	relayAjaxApiKey({
-  		url: config.service_url+'pengeluaran/strict/tbp/index/0?is_panjar=1&jenis=UP&status='+status,
+  		url: config.service_url+'pengeluaran/strict/tbp/index/0?status='+status,
+		//   url: config.service_url+'pengeluaran/strict/tbp/index/0?is_panjar=1&jenis=UP&status='+status,
 	  	type: 'get',
 	  	success: function (response) {
 			console.log('TBP', response);
@@ -22,7 +23,7 @@ function singkron_tbp_lokal() {
 						// melakukan reset page sesuai data per skpd
 						current_data.page = page_skpd[current_data.id_skpd].length;
 
-						singkron_spp_ke_lokal_skpd(current_data, type_data, status, ()=>{
+						singkron_tbp_ke_lokal_skpd(current_data, ()=>{
 							resolve_reduce(nextData);
 				  		});
 					})
@@ -38,110 +39,98 @@ function singkron_tbp_lokal() {
 			}, Promise.resolve(response[last]))
 			.then(function (data_last) {
 			  	jQuery("#wrap-loading").hide();
-			  	alert("Berhasil singkron SPP");
+			  	alert("Berhasil singkron TBP");
 			});
 	  	},
 	});
 }
 
-function singkron_spp_ke_lokal_skpd(current_data, tipe, status, callback) {
-	var spp = {
-	  	action: "singkron_spp",
+function singkron_tbp_ke_lokal_skpd(current_data, callback) {
+	var tbp = {
+	  	action: "singkron_tbp",
 	  	tahun_anggaran: _token.tahun,
 	  	api_key: config.api_key,
 	  	idSkpd: current_data.id_skpd,
-	  	tipe: tipe,
+	  	jenis: current_data.jenis_tbp,
 	  	sumber: 'ri',
 	  	page: current_data.page,
-	 	data: {}
+		data: {}
 	};   
-	spp.data[0] = {}
-	spp.data[0].nomorSpp = current_data.nomor_spp;
-	spp.data[0].nilaiSpp = current_data.nilai_spp;
-	spp.data[0].tanggalSpp = current_data.tanggal_spp;
-	spp.data[0].keteranganSpp = current_data.keterangan_spp;
-	spp.data[0].idSkpd = current_data.id_skpd;
-	spp.data[0].idSubUnit = current_data.id_sub_skpd;
-	spp.data[0].nilaiDisetujuiSpp = current_data.nilai_spp;
-	spp.data[0].tanggalDisetujuiSpp = current_data.id_sub_giat;
-	spp.data[0].jenisSpp = current_data.jenis_spp;
-	spp.data[0].verifikasiSpp = current_data.is_verifikasi_spp;
-	spp.data[0].keteranganVerifikasi = current_data.keterangan_verifikasi_spp;
-	// spp.data[0].idSpd = current_data.id_spd;
-	// spp.data[0].idPengesahanSpj = id_sub_giat;
-	spp.data[0].kunciRekening = current_data.is_kunci_rekening_spp;
-	// spp.data[0].alamatPenerimaSpp = id_sub_giat;
-	// spp.data[0].bankPenerimaSpp = id_sub_giat;
-	// spp.data[0].nomorRekeningPenerimaSpp = id_sub_giat;
-	// spp.data[0].npwpPenerimaSpp = id_sub_giat;
-	spp.data[0].idUser = current_data.created_by;
-	spp.data[0].jenisLs = current_data.jenis_ls_spp;
-	spp.data[0].isUploaded = current_data.is_rekanan_upload;
-	spp.data[0].tahunSpp = current_data.tahun;
-	spp.data[0].idKontrak = current_data.id_kontrak;
-	spp.data[0].idBA = current_data.id_ba;
-	spp.data[0].created_at = current_data.created_at;
-	spp.data[0].updated_at = current_data.updated_at;
-	spp.data[0].isSpm = current_data.is_spm;
-	spp.data[0].statusPerubahan = current_data.status_perubahan_by;
-	// spp.data[0].isDraft = id_sub_giat;
-	spp.data[0].idSpp = current_data.id_spp;
-	// spp.data[0].kodeDaerah = id_sub_giat;
-	spp.data[0].idDaerah = current_data.id_daerah;
-	spp.data[0].isGaji = current_data.is_gaji;
-	spp.data[0].is_sptjm = current_data.is_sptjm;
-	spp.data[0].tanggal_otorisasi = current_data.verifikasi_spp_at;
-	spp.data[0].is_otorisasi = current_data.is_verifikasi_spp;
-	spp.data[0].bulan_gaji = current_data.bulan_gaji;
-	spp.data[0].id_pegawai_pptk = current_data.id_pegawai_pptk;
-	// spp.data[0].nama_pegawai_pptk = id_sub_giat;
-	// spp.data[0].nip_pegawai_pptk = id_sub_giat;
-	spp.data[0].id_jadwal = current_data.id_jadwal;
-	spp.data[0].id_tahap = current_data.id_tahap;
-	spp.data[0].status_tahap = current_data.status_tahap;
-	spp.data[0].kode_tahap = current_data.kode_tahap;
-	spp.data[0].is_tpp = current_data.is_tpp;
-	spp.data[0].bulan_tpp = current_data.bulan_tpp;
-	spp.data[0].id_pengajuan_tu = current_data.id_pengajuan_tu;
-	// spp.data[0].nomor_pengajuan_tu = id_sub_giat;
-	// spp.data[0].id_npd = id_sub_giat;
+	tbp.data[0] = {}
+	tbp.data[0].id_tbp = current_data.id_tbp;
+	tbp.data[0].id_sp2d_distribusi = current_data.id_sp2d_distribusi;
+	tbp.data[0].id_sp2d = current_data.id_sp2d;
+	tbp.data[0].tahun = current_data.tahun;
+	tbp.data[0].id_daerah = current_data.id_daerah;
+	tbp.data[0].id_unit = current_data.id_unit;
+	tbp.data[0].id_skpd = current_data.id_skpd;
+	tbp.data[0].id_sub_skpd = current_data.id_sub_skpd;
+	tbp.data[0].nilai_tbp = current_data.nilai_tbp;
+	tbp.data[0].tanggal_tbp = current_data.tanggal_tbp;
+	tbp.data[0].keterangan_tbp = current_data.keterangan_tbp;
+	tbp.data[0].nilai_materai_tbp = current_data.nilai_materai_tbp;
+	tbp.data[0].nomor_kwitansi = current_data.nomor_kwitansi;
+	tbp.data[0].id_pegawai_pa_kpa = current_data.id_pegawai_pa_kpa;
+	tbp.data[0].jenis_tbp = current_data.jenis_tbp;
+	tbp.data[0].jenis_ls_tbp = current_data.jenis_ls_tbp;
+	tbp.data[0].is_kunci_rekening_tbp = current_data.is_kunci_rekening_tbp;
+	tbp.data[0].is_panjar = current_data.is_panjar;
+	tbp.data[0].is_lpj = current_data.is_lpj;
+	tbp.data[0].id_lpj = current_data.id_lpj;
+	tbp.data[0].id_npd = current_data.id_npd;
+	tbp.data[0].is_rekanan_upload = current_data.is_rekanan_upload;
+	tbp.data[0].status_aklap = current_data.status_aklap;
+	tbp.data[0].nomor_jurnal = current_data.nomor_jurnal;
+	tbp.data[0].jurnal_id = current_data.jurnal_id;
+	tbp.data[0].metode = current_data.metode;
+	tbp.data[0].id_jadwal = current_data.id_jadwal;
+	tbp.data[0].id_tahap = current_data.id_tahap;
+	tbp.data[0].status_tahap = current_data.status_tahap;
+	tbp.data[0].kode_tahap = current_data.kode_tahap;
+	tbp.data[0].created_at = current_data.created_at;
+	tbp.data[0].created_by = current_data.created_by;
+	tbp.data[0].kode_skpd = current_data.kode_skpd;
+	tbp.data[0].nama_skpd = current_data.nama_skpd;
+	tbp.data[0].kode_sub_skpd = current_data.kode_sub_skpd;
+	tbp.data[0].nama_sub_skpd = current_data.nama_sub_skpd;
+	tbp.data[0].total_pertanggungjawaban = current_data.total_pertanggungjawaban;
 	var data_back = {
 	 	 message: {
 			type: "get-url",
 			content: {
 			  	url: config.url_server_lokal,
 			  	type: "post",
-			  	data: spp,
+			  	data: tbp,
 			  	return: false
 			},
 	  	},
 	};
 	chrome.runtime.sendMessage(data_back, (resp) => {
-	  	pesan_loading("Kirim data SPP ID SKPD="+current_data.id_skpd+" tipe="+tipe+" status="+status+" nomor="+current_data.nomor_spp);
+	  	pesan_loading("Kirim data TBP ID SKPD="+current_data.id_skpd+" jenis="+current_data.jenis_tbp+" nomor="+current_data.nomor_tbp);
 	});
-	if(tipe == 'UP'){
-		return callback();
-	}
+	// if(current_data.jenis_tbp == 'UP'){
+	// 	return callback();
+	// }
 
 	new Promise(function (resolve, reject) {
-		jQuery.ajax({
-			url: config.service_url + "pengeluaran/strict/spp/pembuatan/cetak/" + current_data.id_spp,
+        jQuery.ajax({
+            url: config.service_url + "pengeluaran/strict/tbp/cetak/" + current_data.id_tbp,
 			type: 'get',
 			dataType: "JSON",
 			beforeSend: function (xhr) {			    
 				xhr.setRequestHeader("Authorization", 'Bearer '+getCookie('X-SIPD-PU-TK'));
 			},
-			success: function (res) {
-				console.log('response detail spp', res);
-				var spp_detail = {
-					action: "singkron_spp_detail",
+            success: function (res) {
+                console.log('response detail tbp', res);
+				var tbp_detail = {
+					action: "singkron_tbp_detail",
 					tahun_anggaran: _token.tahun,
 					api_key: config.api_key,
 					idSkpd: current_data.id_skpd,
-					id_spp: current_data.id_spp,
-					tipe: tipe,
+					id_tbp: current_data.id_tbp,
+					jenis: current_data.jenis_tbp,
 					sumber: 'ri',
-					data: res[res.jenis.toLowerCase()]
+					data: res
 				};
 				var data_back = {
 					message: {
@@ -149,25 +138,26 @@ function singkron_spp_ke_lokal_skpd(current_data, tipe, status, callback) {
 						content: {
 							url: config.url_server_lokal,
 							type: "post",
-							data: spp_detail,
-							return: true
+							data: tbp_detail,
+							return: false
 						},
 					}
 				};
-				chrome.runtime.sendMessage(data_back, (resp) => {
-					window.singkron_spp_detail = {
-						resolve: resolve
-					};
-					pesan_loading("Kirim data SPP detail ID="+current_data.id_spp+" tipe="+tipe);
-				});
-			},
-			error: function(err){
-				console.log('Error get detail SPP! id='+current_data.id_spp, err);
-				resolve();
-			}
-	  	});
-  	})
-  	.then(function () {
-	  	callback();
-	});
+                chrome.runtime.sendMessage(data_back, (resp) => {
+                    // window.singkron_tbp_detail = {
+                    //     resolve: resolve
+                    // };
+                    pesan_loading("Kirim data TBP detail ID="+current_data.id_tbp);
+					return callback();
+                });
+            },
+            error: function(err){
+                console.log('Error get detail TBP! id='+current_data.id_tbp, err);
+                resolve();
+            }
+        });
+    })
+    .then(function () {
+        callback();
+    });
 }
