a152086760c329deba97da5b8e61cf19b7ed2502
Saiful Bangil <saifulbangil@gmail.com>
2024-11-02 02:33:33 +0700

    filter bulan sp2d

--- js/content/pengeluaran/sp2d/sp2d.js
+++ js/content/pengeluaran/sp2d/sp2d.js
@@ -3,12 +3,15 @@ function singkron_sp2d_lokal(bulan, data=['UP', 'LS', 'GU', 'TU' ]){
 	pesan_loading('Get data SP2D Bulan '+bulan);
 	var bulan = bulan;
 	console.log(bulan);
-	if(bulan == '' || bulan == undefined || bulan == 0){
+	if(bulan == '' || bulan == 'undefined' || bulan == 0){
 		return alert('Bulan Belum dipilih !!!');
 	}
 	else if(bulan == 'LS,GU,TU')
 	{
-		resolve();
+		// resolve();
+		var bulan = bulan;
+		// jQuery("#wrap-loading").hide();
+	  	// alert("Berhasil singkron SP2D");
 	}
 	var status = 'ditransfer';
 	var type_data = data.shift();
@@ -59,7 +62,7 @@ function singkron_sp2d_lokal(bulan, data=['UP', 'LS', 'GU', 'TU' ]){
 function singkron_sp2d_lokal_per_jenis(type_data, bulan, status, page=1, response_all=[], cb){
     pesan_loading('Get data SP2D Bulan "'+bulan+'" jenis='+type_data+', status='+status+', halaman='+page);
     relayAjaxApiKey({
-        url: config.service_url+'pengeluaran/strict/sp2d/pembuatan/index?jenis='+type_data+'&status='+status+'&page='+page+'&nomor_sp2d='+bulan+'/'+_token.tahun,
+        url: config.service_url+'pengeluaran/strict/sp2d/pembuatan/index?jenis='+type_data+'&status='+status+'&page='+page+'&nomor_sp2d=/'+bulan+'/'+_token.tahun,
         type: 'get',
         success: function (response) {
             console.log('SP2D', response);
@@ -89,73 +92,74 @@ function singkron_sp2d_ke_lokal_skpd(current_data, tipe, bulan, status, callback
 	  	tipe: tipe,
 	  	sumber: 'ri',
 	  	page: current_data.page,
-	 	data: {}
+	 	data: []
 	};
-	sp2d.data[0] = {};
-	sp2d.data[0].bulan_gaji = current_data.bulan_gaji;
-	sp2d.data[0].bulan_tpp = current_data.bulan_tpp;
-	sp2d.data[0].created_at = current_data.created_at;
-	sp2d.data[0].created_by = current_data.created_by;
-	sp2d.data[0].deleted_at = current_data.deleted_at;
-	sp2d.data[0].deleted_by = current_data.deleted_by;
-	sp2d.data[0].id_bank = current_data.id_bank;
-	sp2d.data[0].id_daerah = current_data.id_daerah;
-	sp2d.data[0].id_jadwal = current_data.id_jadwal;
-	sp2d.data[0].id_pegawai_bud_kbud = current_data.id_pegawai_bud_kbud;
-	sp2d.data[0].id_rkud = current_data.id_rkud;
-	sp2d.data[0].id_skpd = current_data.id_skpd;
-	sp2d.data[0].id_sp_2_d = current_data.id_sp_2_d;
-	sp2d.data[0].id_spm = current_data.id_spm;
-	sp2d.data[0].id_sub_skpd = current_data.id_sub_skpd;
-	sp2d.data[0].id_sumber_dana = current_data.id_sumber_dana;
-	sp2d.data[0].id_tahap = current_data.id_tahap;
-	sp2d.data[0].id_unit = current_data.id_unit;
-	sp2d.data[0].is_gaji = current_data.is_gaji;
-	sp2d.data[0].is_kunci_rekening_sp_2_d = current_data.is_kunci_rekening_sp_2_d;
-	sp2d.data[0].is_pelimpahan = current_data.is_pelimpahan;
-	sp2d.data[0].is_status_perubahan = current_data.is_status_perubahan;
-	sp2d.data[0].is_tpp = current_data.is_tpp;
-	sp2d.data[0].is_transfer_sp_2_d = current_data.is_transfer_sp_2_d;
-	sp2d.data[0].is_verifikasi_sp_2_d = current_data.is_verifikasi_sp_2_d;
-	sp2d.data[0].jenis_gaji = current_data.jenis_gaji;
-	sp2d.data[0].jenis_ls_sp_2_d = current_data.jenis_ls_sp_2_d;
-	sp2d.data[0].jenis_rkud = current_data.jenis_rkud;
-	sp2d.data[0].jenis_sp_2_d = current_data.jenis_sp_2_d;
-	sp2d.data[0].jurnal_id = current_data.jurnal_id;
-	sp2d.data[0].keterangan_sp_2_d = current_data.keterangan_sp_2_d;
-	sp2d.data[0].keterangan_transfer_sp_2_d = current_data.keterangan_transfer_sp_2_d;
-	sp2d.data[0].keterangan_verifikasi_sp_2_d = current_data.keterangan_verifikasi_sp_2_d;
-	sp2d.data[0].kode_skpd = current_data.kode_skpd;
-	sp2d.data[0].kode_sub_skpd = current_data.kode_sub_skpd;
-	sp2d.data[0].kode_tahap = current_data.kode_tahap;
-	sp2d.data[0].metode = current_data.metode;
-	sp2d.data[0].nama_bank = current_data.nama_bank;
-	sp2d.data[0].nama_bud_kbud = current_data.nama_bud_kbud;
-	sp2d.data[0].nama_rek_bp_bpp = current_data.nama_rek_bp_bpp;
-	sp2d.data[0].nama_skpd = current_data.nama_skpd;
-	sp2d.data[0].nama_sub_skpd = current_data.nama_sub_skpd;
-	sp2d.data[0].nilai_materai_sp_2_d = current_data.nilai_materai_sp_2_d;
-	sp2d.data[0].nilai_sp_2_d = current_data.nilai_sp_2_d;
-	sp2d.data[0].nip_bud_kbud = current_data.nip_bud_kbud;
-	sp2d.data[0].no_rek_bp_bpp = current_data.no_rek_bp_bpp;
-	sp2d.data[0].nomor_jurnal = current_data.nomor_jurnal;
-	sp2d.data[0].nomor_sp_2_d = current_data.nomor_sp_2_d;
-	sp2d.data[0].nomor_spm = current_data.nomor_spm;
-	sp2d.data[0].status_aklap = current_data.status_aklap;
-	sp2d.data[0].status_perubahan_at = current_data.status_perubahan_at;
-	sp2d.data[0].status_perubahan_by = current_data.status_perubahan_by;
-	sp2d.data[0].status_tahap = current_data.status_tahap;
-	sp2d.data[0].tahun = current_data.tahun;
-	sp2d.data[0].tahun_gaji = current_data.tahun_gaji;
-	sp2d.data[0].tahun_tpp = current_data.tahun_tpp;
-	sp2d.data[0].tanggal_sp_2_d = current_data.tanggal_sp_2_d;
-	sp2d.data[0].tanggal_spm = current_data.tanggal_spm;
-	sp2d.data[0].transfer_sp_2_d_at = current_data.transfer_sp_2_d_at;
-	sp2d.data[0].transfer_sp_2_d_by = current_data.transfer_sp_2_d_by;
-	sp2d.data[0].updated_at = current_data.updated_at;
-	sp2d.data[0].updated_by = current_data.updated_by;
-	sp2d.data[0].verifikasi_sp_2_d_at = current_data.verifikasi_sp_2_d_at;
-	sp2d.data[0].verifikasi_sp_2_d_by = current_data.verifikasi_sp_2_d_by;
+	console.log('singkron sp2d',current_data);
+	sp2d.data = {};
+	sp2d.data.bulan_gaji = current_data.bulan_gaji;
+	sp2d.data.bulan_tpp = current_data.bulan_tpp;
+	sp2d.data.created_at = current_data.created_at;
+	sp2d.data.created_by = current_data.created_by;
+	sp2d.data.deleted_at = current_data.deleted_at;
+	sp2d.data.deleted_by = current_data.deleted_by;
+	sp2d.data.id_bank = current_data.id_bank;
+	sp2d.data.id_daerah = current_data.id_daerah;
+	sp2d.data.id_jadwal = current_data.id_jadwal;
+	sp2d.data.id_pegawai_bud_kbud = current_data.id_pegawai_bud_kbud;
+	sp2d.data.id_rkud = current_data.id_rkud;
+	sp2d.data.id_skpd = current_data.id_skpd;
+	sp2d.data.id_sp_2_d = current_data.id_sp_2_d;
+	sp2d.data.id_spm = current_data.id_spm;
+	sp2d.data.id_sub_skpd = current_data.id_sub_skpd;
+	sp2d.data.id_sumber_dana = current_data.id_sumber_dana;
+	sp2d.data.id_tahap = current_data.id_tahap;
+	sp2d.data.id_unit = current_data.id_unit;
+	sp2d.data.is_gaji = current_data.is_gaji;
+	sp2d.data.is_kunci_rekening_sp_2_d = current_data.is_kunci_rekening_sp_2_d;
+	sp2d.data.is_pelimpahan = current_data.is_pelimpahan;
+	sp2d.data.is_status_perubahan = current_data.is_status_perubahan;
+	sp2d.data.is_tpp = current_data.is_tpp;
+	sp2d.data.is_transfer_sp_2_d = current_data.is_transfer_sp_2_d;
+	sp2d.data.is_verifikasi_sp_2_d = current_data.is_verifikasi_sp_2_d;
+	sp2d.data.jenis_gaji = current_data.jenis_gaji;
+	sp2d.data.jenis_ls_sp_2_d = current_data.jenis_ls_sp_2_d;
+	sp2d.data.jenis_rkud = current_data.jenis_rkud;
+	sp2d.data.jenis_sp_2_d = current_data.jenis_sp_2_d;
+	sp2d.data.jurnal_id = current_data.jurnal_id;
+	sp2d.data.keterangan_sp_2_d = current_data.keterangan_sp_2_d;
+	sp2d.data.keterangan_transfer_sp_2_d = current_data.keterangan_transfer_sp_2_d;
+	sp2d.data.keterangan_verifikasi_sp_2_d = current_data.keterangan_verifikasi_sp_2_d;
+	sp2d.data.kode_skpd = current_data.kode_skpd;
+	sp2d.data.kode_sub_skpd = current_data.kode_sub_skpd;
+	sp2d.data.kode_tahap = current_data.kode_tahap;
+	sp2d.data.metode = current_data.metode;
+	sp2d.data.nama_bank = current_data.nama_bank;
+	sp2d.data.nama_bud_kbud = current_data.nama_bud_kbud;
+	sp2d.data.nama_rek_bp_bpp = current_data.nama_rek_bp_bpp;
+	sp2d.data.nama_skpd = current_data.nama_skpd;
+	sp2d.data.nama_sub_skpd = current_data.nama_sub_skpd;
+	sp2d.data.nilai_materai_sp_2_d = current_data.nilai_materai_sp_2_d;
+	sp2d.data.nilai_sp_2_d = current_data.nilai_sp_2_d;
+	sp2d.data.nip_bud_kbud = current_data.nip_bud_kbud;
+	sp2d.data.no_rek_bp_bpp = current_data.no_rek_bp_bpp;
+	sp2d.data.nomor_jurnal = current_data.nomor_jurnal;
+	sp2d.data.nomor_sp_2_d = current_data.nomor_sp_2_d;
+	sp2d.data.nomor_spm = current_data.nomor_spm;
+	sp2d.data.status_aklap = current_data.status_aklap;
+	sp2d.data.status_perubahan_at = current_data.status_perubahan_at;
+	sp2d.data.status_perubahan_by = current_data.status_perubahan_by;
+	sp2d.data.status_tahap = current_data.status_tahap;
+	sp2d.data.tahun = current_data.tahun;
+	sp2d.data.tahun_gaji = current_data.tahun_gaji;
+	sp2d.data.tahun_tpp = current_data.tahun_tpp;
+	sp2d.data.tanggal_sp_2_d = current_data.tanggal_sp_2_d;
+	sp2d.data.tanggal_spm = current_data.tanggal_spm;
+	sp2d.data.transfer_sp_2_d_at = current_data.transfer_sp_2_d_at;
+	sp2d.data.transfer_sp_2_d_by = current_data.transfer_sp_2_d_by;
+	sp2d.data.updated_at = current_data.updated_at;
+	sp2d.data.updated_by = current_data.updated_by;
+	sp2d.data.verifikasi_sp_2_d_at = current_data.verifikasi_sp_2_d_at;
+	sp2d.data.verifikasi_sp_2_d_by = current_data.verifikasi_sp_2_d_by;
 	var data_back = {
 	 	 message: {
 			type: "get-url",
